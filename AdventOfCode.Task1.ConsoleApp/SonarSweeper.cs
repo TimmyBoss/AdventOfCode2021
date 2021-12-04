d9fdc3a5bb34dea63407681c40e8e1159058781c
Timmay-26 <ttjharte@ziggo.nl>
2021-12-04 15:38:10 +0100

    Refactored puzzle 2 day 1

--- AdventOfCode.Task1.ConsoleApp/SonarSweeper.cs
+++ AdventOfCode.Task1.ConsoleApp/SonarSweeper.cs
@@ -12,10 +12,37 @@ namespace AdventOfCode.Task1.ConsoleApp
 
         public int GetAnswer1()
         {
+            return GetIncreasedCount(_depthList);
+        }
+
+        public int GetAnswer2()
+        {
+            var measurementWindows = new List<int>();
+            int counter = 0;
+
+            while (counter < _depthList.Count-2)
+            {
+                var n1 = _depthList[counter];
+                var n2 = _depthList[counter + 1];
+                var n3 = _depthList[counter + 2];
+                measurementWindows.Add(n1 + n2 + n3);
+                counter++;
+            }
+
+            return GetIncreasedCount(measurementWindows);
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _depthList = puzzleInput.Select(int.Parse).ToList();
+        }
+
+        private int GetIncreasedCount(List<int> depthList)
+        {
             var increasedCount = 0;
             int? prevDepth = null;
 
-            foreach (var depth in _depthList)
+            foreach (var depth in depthList)
             {
                 if (prevDepth.HasValue)
                 {
@@ -27,16 +54,6 @@ namespace AdventOfCode.Task1.ConsoleApp
             }
 
             return increasedCount;
-        }
-
-        public int GetAnswer2()
-        {
-            return 0;
-        }
-
-        public void SetupPuzzleInput(List<string> puzzleInput)
-        {
-            _depthList = puzzleInput.Select(int.Parse).ToList();
         }
     }
 }
