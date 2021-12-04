899c804a592922309973a605f8923563b8f64261
Timmay-26 <ttjharte@ziggo.nl>
2021-12-04 15:16:16 +0100

    Refactored Day 1 puzzle 1

--- AdventOfCode.Task1.ConsoleApp/Program.cs
+++ AdventOfCode.Task1.ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
-﻿using AdventOfCode.Console.Task1;
-using System;
+﻿using AdventOfCode.Shared;
 
 namespace AdventOfCode.Task1.ConsoleApp
 {
@@ -7,23 +6,8 @@ namespace AdventOfCode.Task1.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var numberCollection = new NumberCollection();
-            var numbers = numberCollection.GetSumsOfThree();
-            int? prevNumber = null;
-            var largerCount = 0;
-
-            foreach (var number in numbers)
-            {
-                if (prevNumber.HasValue)
-                {
-                    if (number > prevNumber)
-                        largerCount++;
-                }
-                prevNumber = number;
-            }
-
-            System.Console.WriteLine(largerCount);
-            System.Console.ReadLine();
+            var task = new Task<SonarSweeper>();
+            task.ReadAnswers();
         }
     }
 }
---
+++ AdventOfCode.Task1.ConsoleApp/SonarSweeper.cs
@@ -0,0 +1,42 @@
+﻿using AdventOfCode.Shared;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Task1.ConsoleApp
+{
+    public class SonarSweeper : ITask
+    {
+        private List<int> _depthList;
+
+        public SonarSweeper() { }
+
+        public int GetAnswer1()
+        {
+            var increasedCount = 0;
+            int? prevDepth = null;
+
+            foreach (var depth in _depthList)
+            {
+                if (prevDepth.HasValue)
+                {
+                    if (prevDepth < depth)
+                        increasedCount++;
+                }
+
+                prevDepth = depth;
+            }
+
+            return increasedCount;
+        }
+
+        public int GetAnswer2()
+        {
+            return 0;
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _depthList = puzzleInput.Select(int.Parse).ToList();
+        }
+    }
+}
