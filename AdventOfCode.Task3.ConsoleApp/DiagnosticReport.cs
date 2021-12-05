b9f2a598ab99fa6d58e0fdc791d9b0e0cede0410
Timmay-26 <ttjharte@ziggo.nl>
2021-12-05 20:22:37 +0100

    Added day 3 task 1

---
+++ AdventOfCode.Task3.ConsoleApp/DiagnosticReport.cs
@@ -0,0 +1,44 @@
+﻿using AdventOfCode.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Task3.ConsoleApp
+{
+    public class DiagnosticReport : ITask
+    {
+        private readonly int binaryLength = 12;
+
+        private List<string> _binaryNumbers;
+
+        public int GetAnswer1()
+        {
+            var gammaRate = "";
+            var epsilonRate = "";
+
+            for (int i = 0; i < binaryLength; i++)
+            {
+                var ones = _binaryNumbers.Count(b => b[i] == '1');
+                var zeros = _binaryNumbers.Count(b => b[i] == '0');
+
+                gammaRate += ones > zeros ? "1" : "0";
+                epsilonRate += ones > zeros ? "0" : "1";
+            }
+
+            var gammaDecimal = Convert.ToInt32(gammaRate, 2);
+            var epsilonDecimal = Convert.ToInt32(epsilonRate, 2);
+
+            return gammaDecimal * epsilonDecimal;
+        }
+
+        public int GetAnswer2()
+        {
+            return 0;
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _binaryNumbers = puzzleInput;
+        }
+    }
+}
---
+++ AdventOfCode.Task3.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCode.Task3.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task<DiagnosticReport>();
+            task.ReadAnswers();
+        }
+    }
+}
