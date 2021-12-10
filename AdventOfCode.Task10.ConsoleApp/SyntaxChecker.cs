b24e373f5a3d244ea5622f7f4a4218f9b227c30f
Tim Harte <tim@scorito.com>
2021-12-10 14:03:51 +0100

    Day 9 task 1 finished

--- AdventOfCode.Task10.ConsoleApp/SyntaxChecker.cs
+++ AdventOfCode.Task10.ConsoleApp/SyntaxChecker.cs
@@ -1,5 +1,8 @@
 ﻿using AdventOfCode.Shared;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace AdventOfCode.Task10.ConsoleApp
 {
@@ -7,18 +10,97 @@ namespace AdventOfCode.Task10.ConsoleApp
     public class SyntaxChecker : ITask
     {
         private List<string> _puzzleInput;
+        private Dictionary<string, int> _charList;
+
+
+        private readonly char haakjeOpen = '(';
+        private readonly char haakjeClosed = ')';
+        private readonly char bracketOpen = '[';
+        private readonly char bracketClosed = ']';
+        private readonly char accoladeOpen = '{';
+        private readonly char accodeladeClosed = '}';
+        private readonly char henkOpen = '<';
+        private readonly char henkClosed = '>';
+
+        public SyntaxChecker()
+        {
+            _charList = new Dictionary<string, int>();
+            _charList.Add(")", 3);
+            _charList.Add("]", 57);
+            _charList.Add("}", 1197);
+            _charList.Add(">", 25137);
+        }
 
         public long GetAnswer1()
         {
             var invalidSyntaxList = new List<string>();
+            var haakjeCount = 0;
+            var bracketCount = 0;
+            var accoladeCount = 0;
+            var henkCount = 0;
+
             foreach (var item in _puzzleInput)
             {
-                if (item.ToCharArray().Length % 2 == 0)
+                var i = 0;
+                var sb = new StringBuilder(item);
+
+                while (i < sb.Length-1)
+                {
+                    if (sb[i] == haakjeOpen && sb[i + 1] == haakjeClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == bracketOpen && sb[i + 1] == bracketClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == accoladeOpen && sb[i + 1] == accodeladeClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == henkOpen && sb[i + 1] == henkClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+
+                    i++;     
+                }
+
+                if (sb.ToString().Contains(haakjeClosed) || sb.ToString().Contains(bracketClosed) ||
+                    sb.ToString().Contains(accodeladeClosed) || sb.ToString().Contains(henkClosed)) 
+                {
+                    foreach (var c in sb.ToString())
+                    {
+                        if (c == ')')
+                        {
+                            haakjeCount++;
+                            break;
+                        }
+                        if (c == ']')
+                        {
+                            bracketCount++;
+                            break;
+                        }
+                        if (c == '}')
+                        {
+                            accoladeCount++;
+                            break;
+                        }
+                        if (c == '>')
+                        {
+                            henkCount++;
+                            break;
+                        }
+                    }
                     invalidSyntaxList.Add(item);
-            
+                }
             }
 
-            return 0;
+            return (haakjeCount*3) + (bracketCount*57) + (accoladeCount*1197) + (henkCount*25137);
         }
 
         public long GetAnswer2()
