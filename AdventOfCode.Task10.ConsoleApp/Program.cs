17a8586b5b4f95fa174f380e23ed385fc9726fb5
Timmay-26 <ttjharte@ziggo.nl>
2021-12-10 07:42:10 +0100

    Day 10 in progress

---
+++ AdventOfCode.Task10.ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using AdventOfCode.Shared;
+
+namespace AdventOfCode.Task10.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new Task<SyntaxChecker>();
+            task.ReadAnswers();
+        }
+    }
+}
---
+++ AdventOfCode.Task10.ConsoleApp/SyntaxChecker.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Shared;
+using System.Collections.Generic;
+
+namespace AdventOfCode.Task10.ConsoleApp
+{
+
+    public class SyntaxChecker : ITask
+    {
+        private List<string> _puzzleInput;
+
+        public long GetAnswer1()
+        {
+            var invalidSyntaxList = new List<string>();
+            foreach (var item in _puzzleInput)
+            {
+                if (item.ToCharArray().Length % 2 == 0)
+                    invalidSyntaxList.Add(item);
+            
+            }
+
+            return 0;
+        }
+
+        public long GetAnswer2()
+        {
+            return 0;
+        }
+
+        public void SetupPuzzleInput(List<string> puzzleInput)
+        {
+            _puzzleInput = puzzleInput;
+        }
+    }
+}
