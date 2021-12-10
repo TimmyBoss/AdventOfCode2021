1b9ca8286f4adb25f9b26c22172a81c5d075db16
Timmay-26 <ttjharte@ziggo.nl>
2021-12-10 19:48:10 +0100

    Finished code

--- AdventOfCode.Task10.ConsoleApp/SyntaxChecker.cs
+++ AdventOfCode.Task10.ConsoleApp/SyntaxChecker.cs
@@ -105,7 +105,59 @@ namespace AdventOfCode.Task10.ConsoleApp
 
         public long GetAnswer2()
         {
-            return 0;
+            var incompleteSyntaxList = new List<long>();
+            foreach (var item in _puzzleInput)
+            {
+                long sum = 0;
+                var i = 0;
+                var sb = new StringBuilder(item);
+
+                while (i < sb.Length - 1)
+                {
+                    if (sb[i] == haakjeOpen && sb[i + 1] == haakjeClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == bracketOpen && sb[i + 1] == bracketClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == accoladeOpen && sb[i + 1] == accodeladeClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+                    if (sb[i] == henkOpen && sb[i + 1] == henkClosed)
+                    {
+                        sb.Remove(i, 2);
+                        i = 0;
+                    }
+
+                    i++;
+                }
+
+                if (!sb.ToString().Contains(haakjeClosed) && !sb.ToString().Contains(bracketClosed) &&
+                    !sb.ToString().Contains(accodeladeClosed) && !sb.ToString().Contains(henkClosed))
+                {
+                    for (var c = sb.Length-1; c >= 0; c--)
+                    {
+                        if (sb[c].ToString() == "(")
+                            sum = (sum * 5) + 1;
+                        if (sb[c].ToString() == "[")
+                            sum = (sum * 5) + 2;
+                        if (sb[c].ToString() == "{")
+                            sum = (sum * 5) + 3;
+                        if (sb[c].ToString() == "<")
+                            sum = (sum * 5) + 4;
+                    }
+                    incompleteSyntaxList.Add(sum);
+                }
+            }
+            var sorted = incompleteSyntaxList.OrderBy(o => o).ToList();
+
+            return sorted[sorted.Count / 2];
         }
 
         public void SetupPuzzleInput(List<string> puzzleInput)
